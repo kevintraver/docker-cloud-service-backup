15f7017da797415f4a0535a6f3564b4507209b09
Kevin Traver <kevintraver@gmail.com>
2016-07-24 20:23:38 -0600

    When stack is available, backup to file

--- docker_listen.rb
+++ docker_listen.rb
@@ -2,19 +2,30 @@ require 'docker_cloud'
 require 'rest-client'
 
 client = DockerCloud::Client.new(ENV['DOCKER_CLOUD_USERNAME'], ENV['DOCKER_CLOUD_API_KEY'])
+headers = { 'Authorization' => "Basic #{Base64.strict_encode64(ENV['DOCKER_CLOUD_USERNAME'] + ':' + ENV['DOCKER_CLOUD_API_KEY'])}", 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+
 events = client.events
 
+@base_url = "https://cloud.docker.com/"
 
 # Event listeners
 events.on(:message) do |event|
-  uri = "https://cloud.docker.com/" + event.resource_uri
-  headers = { 'Authorization' => "Basic #{Base64.strict_encode64(ENV['DOCKER_CLOUD_USERNAME'] + ':' + ENV['DOCKER_CLOUD_API_KEY'])}", 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+  uri = @base_url + event.resource_uri
   response = JSON.parse(RestClient.get(uri, headers))
-  File.open("responses/#{event.uuid}.json","w") do |f|
-    details = {type: event.type, action: event.action, parents: event.parents, state: event.state}
-    f.write(JSON.pretty_generate(details) + "\n")
-    f.write(JSON.pretty_generate(response))
+  if event.type == "service" && event.action == "update"
+    # File.open("responses/#{event.uuid}.json","w") do |f|
+    #   details = {type: event.type, action: event.action, parents: event.parents, state: event.state}
+    #   f.write(JSON.pretty_generate(details) + "\n")
+    #   f.write(JSON.pretty_generate(response))
+    # end
+    service = client.services.get(response["uuid"])
+    if stack = service.stack
+      File.open("config/#{stack.name}.yaml","w") do |f|
+        f.write(stack.export.to_yaml)
+      end
+    end
   end
+
 
 end
 
